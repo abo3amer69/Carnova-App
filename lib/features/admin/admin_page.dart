48457b98395a1bb3e1f1bd12539d3871b8871f34
Ahmed Amer <AhmedAmer>
2025-05-12 20:39:30 +0300

    create stuructre the app and add package we use and add icon lanucher to app

---
+++ lib/features/admin/admin_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AdminPageView extends StatelessWidget {
+  const AdminPageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
